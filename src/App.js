c2453363e3816a5a7b7238934e540708949da05f
Giorgio Ventura <gventuraagramonte@gmail.com>
2019-12-16 17:40:26 -0500

    using hooks and other components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react'
-import { Category } from './components/Category'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyle } from './styles/GlobalStyles'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
-  <Category />
+  <div>
+    <GlobalStyle />
+    <Logo />
+    <ListOfCategories />
+    <ListOfPhotoCards />
+  </div>
 )
